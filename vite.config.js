19db8bb8a6b96adb4132a5ec7dcd7c9cdd0eba10
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2026-07-13 22:47:42 +0300

    Add favicon.ico and clean up vite.config.js by removing unused options

--- vite.config.js
+++ vite.config.js
@@ -2,72 +2,8 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import { fileURLToPath, URL } from 'node:url';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
-import path from 'path'
-
-
-const DEFAULT_OPTIONS = {
-  logStats: true,
-  ansiColors: true,
-  test: /\.(jpe?g|png|gif|tiff|webp|svg|avif)$/i,
-  exclude: undefined,
-  include: undefined,
-  includePublic: true,
-  svg: {
-    multipass: true,
-    plugins: [
-      {
-        name: 'preset-default',
-        params: {
-          overrides: {
-            cleanupNumericValues: false,
-            cleanupIds: {
-              minify: false,
-              remove: false,
-            },
-            convertPathData: false,
-          },
-        },
-      },
-      'sortAttrs',
-      {
-        name: 'addAttributesToSVGElement',
-        params: {
-          attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
-        },
-      },
-    ],
-  },
-  png: {
-    // https://sharp.pixelplumbing.com/api-output#png
-    quality: 100,
-  },
-  jpeg: {
-    // https://sharp.pixelplumbing.com/api-output#jpeg
-    quality: 100,
-  },
-  jpg: {
-    // https://sharp.pixelplumbing.com/api-output#jpeg
-    quality: 100,
-  },
-  tiff: {
-    // https://sharp.pixelplumbing.com/api-output#tiff
-    quality: 100,
-  },
-  // gif does not support lossless compression
-  // https://sharp.pixelplumbing.com/api-output#gif
-  gif: {},
-  webp: {
-    // https://sharp.pixelplumbing.com/api-output#webp
-    lossless: true,
-  },
-  avif: {
-    // https://sharp.pixelplumbing.com/api-output#avif
-    lossless: true,
-  },
-  cache: false,
-  cacheLocation: undefined,
-};
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
+import path from 'path';
 
 export default defineConfig({
   root: './',
